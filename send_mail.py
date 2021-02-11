023e783fef0ad3c8ab4f2ff9fd438b6aa0d54077
Ole Bause <ole@bausecreative.de>
2021-02-11 21:18:18 +0100

    Update

--- send_mail.py
+++ send_mail.py
@@ -1,22 +1,23 @@
-import smtplib
+import smtplib, ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 #Email Account
-email_sender_account = "jobs@olebause.de"
-email_sender_username = "jobs@olebause.de"
-email_sender_password = "3Lfl*01r"
-email_smtp_server = "olebause.de"
+email_sender_account = "cron@bausecreative.de"
+email_sender_username = "cron@bausecreative.de"
+email_sender_password = "Ruamzuzla9078#"
+email_smtp_server = "mail.bausecreative.de"
 email_smtp_port = 465
 
 #Email Content
-email_recepients = ["info@bausecreative.de"]
+email_recepients = ["info@bausecreative.de", "olebause@hotmail.de"]
 email_subject = "Test"
 email_body = "Test Email."
 
 #login to email server
-server = smtplib.SMTP(email_smtp_server,email_smtp_port)
-server.starttls()
+context = ssl.create_default_context()
+server = smtplib.SMTP_SSL(email_smtp_server, email_smtp_port, context=context)
+#server.starttls()
 server.login(email_sender_username, email_sender_password)
 
 #For loop, sending emails to all email recipients
@@ -28,7 +29,7 @@ for recipient in email_recepients:
     message['Subject'] = email_subject
     message.attach(MIMEText(email_body, 'html'))
     text = message.as_string()
-    server.sendmail(email_sender_account,recipient,text)
+    server.sendmail(email_sender_account, recipient, text)
 
 #All emails sent, log out.
 server.quit()
